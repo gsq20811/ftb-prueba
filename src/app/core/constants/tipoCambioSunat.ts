5901ba0c55d7cf27159cfc559fadc5f461482b89
Guillermo <gsq2081@gmail.com>
2023-08-07 22:25:52 -0500

    refactor(frontend): Modifica tipo de cambio

--- src/app/core/constants/tipoCambioSunat.ts
+++ src/app/core/constants/tipoCambioSunat.ts
@@ -1,16 +1,16 @@
 export const DATOS_SUNAT = [
   {
-    fecha: '28-05-2023',
+    fecha: '2023-05-28',
     venta: 3.675,
     compra: 3.671,
   },
   {
-    fecha: '29-05-2023',
+    fecha: '2023-05-29',
     venta: 3.678,
     compra: 3.674,
   },
   {
-    fecha: '30-05-2023',
+    fecha: '2023-05-30',
     venta: 3.68,
     compra: 3.647,
   },
